5333a0803f2d6d0f38d39b0bb929fd974917b360
Sarvarbek <120723170+SarvarbekUzDev@users.noreply.github.com>
2023-07-24 22:58:02 -0700

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -10,8 +10,9 @@ async def bot_echo(message: types.Message):
 	wait_message = await message.answer("⏳Iltimos biroz kuting...")
 	try:
 		response = ai_response(text=message.text)
-	except:
+	except Exception as e:
 		response = "Qandaydir xatolik❌\n\nAdminlarga murojat qiling"
+		response += f"\n\n{e}"
 	# We delete the word wait
 	await bot.delete_message(chat_id=message.chat.id, message_id=wait_message.message_id)
 	await message.answer(response)
