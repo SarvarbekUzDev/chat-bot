1336e462f4dbb3140a8e96b761b23feb5a22d460
Sarvarbek <120723170+SarvarbekUzDev@users.noreply.github.com>
2023-07-24 23:18:57 -0700

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,6 +1,7 @@
 from aiogram import types
 
 from loader import dp, bot
+from data.config  import get_env
 from functions import ai_response
 
 
@@ -12,7 +13,7 @@ async def bot_echo(message: types.Message):
 		response = ai_response(text=message.text)
 	except Exception as e:
 		response = "Qandaydir xatolik❌\n\nAdminlarga murojat qiling"
-		response += f"\n\n{e}"
+		response += f"\n\n{e}\n\nKEY:   {get_env()['API_KEY']}"
 	# We delete the word wait
 	await bot.delete_message(chat_id=message.chat.id, message_id=wait_message.message_id)
 	await message.answer(response)
